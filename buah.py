fc3b60b1719d8a7b49a0422d90e8f6081da63b0a
melmust <mella.mustika@gmail.com>
2025-07-28 22:18:43 +0700

    Update buah.py

--- buah.py
+++ buah.py
@@ -10,3 +10,16 @@ elif buah == "Jeruk":
     st.image("https://upload.wikimedia.org/wikipedia/commons/c/c4/Orange-Fruit-Pieces.jpg", width=200)
 elif buah == "Pisang":
     st.image("https://upload.wikimedia.org/wikipedia/commons/8/8a/Banana-Single.jpg", width=200)
+
+
+
+import streamlit as st
+
+st.title("Hitung Luas Persegi Panjang")
+
+panjang = st.number_input("Masukkan panjang (cm):")
+lebar = st.number_input("Masukkan lebar (cm):")
+
+luas = panjang * lebar
+st.write("Luasnya adalah", luas, "cm²")
+
